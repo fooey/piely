691dbb17d2a7bb1d64c9178da6048a7f6293e4d7
Jason Rushton <github.com@schtuph.com>
2014-04-29 01:23:58 -0600

    Built a real welcome page along with an Angular based pie chart builder

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,27 @@
 module.exports = function(grunt) {
 
+	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
+		concurrent: {
+			dev: {
+				tasks: ['nodemon:dev', 'watch'],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		},
 
 
 		nodemon: {
@@ -13,8 +29,8 @@ module.exports = function(grunt) {
 				script: './server.js',
 				options: {
 					nodeArgs: ['--harmony'],
-					ext: 'js,jade',
-					ignore: ['node_modules/**', 'gruntfile.js'],
+					ext: 'js,jade,json',
+					ignore: ['node_modules/**', 'public/**', 'views/partials/**', 'gruntfile.js'],
 
 					// delay: 1,
 					env: {
@@ -23,12 +39,138 @@ module.exports = function(grunt) {
 					},
 					cwd: __dirname,
 				}
+			},
+			prod: {
+				script: './server.js',
+				options: {
+					nodeArgs: ['--harmony'],
+					ext: 'js,jade,json',
+					ignore: ['node_modules/**', 'public/**', 'views/partials/**', 'gruntfile.js'],
+
+					// delay: 1,
+					env: {
+						PORT: '31415',
+						NODE_ENV: 'production'
+					},
+					cwd: __dirname,
+				}
 			}
 		},
+
+
+		watch: {
+			templates: {
+				files: [
+					'./views/**/*.jade',
+				],
+				options: {
+					livereload: true,
+				},
+			},
+
+
+			lessApp: {
+				files: [
+					'./public/css/app.less'
+				],
+				tasks: ['less:app'],
+				options: {
+					livereload: false,
+				},
+			},
+
+
+			cssApp: {
+				files: [
+					'./public/css/dist/app.css'
+				],
+				tasks: ['cssmin:app'],
+				options: {
+					livereload: true,
+				},
+			},
+
+			cssAppMin: {
+				files: [
+					'./public/css/dist/app.min.css'
+				],
+				options: {
+					livereload: true,
+				},
+			},
+
+
+			jsDev: {
+				files: [
+					'./public/js/*.js',
+				],
+				// tasks: ['uglify:appJs'],
+				tasks: ['uglify'],
+				options: {
+					livereload: true,
+				},
+			},
+
+			jsProd: {
+				files: [
+					'./public/js/dist/*.min.js',
+				],
+				options: {
+					livereload: true,
+				},
+			},
+		},
+
+
+		less: {
+			app: {
+				files: {
+					"./public/css/dist/app.css":
+						"./public/css/app.less"
+				},
+				options: {
+					sourceMap: true,
+					outputSourceFiles: true,
+					sourceMapURL: './app.css.map',
+					sourceMapFilename: './public/css/dist/app.css.map'
+				}
+			},
+		},
+
+
+		cssmin: {
+			app: {
+				files: {
+					'public/css/dist/app.min.css': [
+						'public/css/dist/app.css',
+					]
+				}
+			}
+		},
+
+		uglify: {
+			options: {
+				report: 'min',
+				stripBanners: false,
+				mangle: true,
+				preserveComments: 'some',
+			},
+			app: {
+				options: {
+					sourceMap: true,
+				},
+				files: {
+					'public/js/dist/app.min.js': [
+						'public/js/app.js',
+					]
+				}
+			},
+		},
+
 	});
 
 
 
-	// grunt.registerTask('minify', ['cssmin:css', 'browserify2:appJs', 'uglify:appJs']);
-	grunt.registerTask('default', ['nodemon']);
+	grunt.registerTask('dev', ['less:app', 'cssmin:app', 'uglify:app', 'concurrent:dev']);
+	grunt.registerTask('default', ['less:app', 'cssmin:app', 'uglify:app', 'nodemon:prod']);
 };
---
+++ public/js/app.js
@@ -0,0 +1,117 @@
+/*!
+*
+*	app
+*
+!*/
+
+angular.module('pieApp', ['ngAnimate'])
+	.controller('AppCtrl', ['$scope', function($scope) {
+		'use strict';
+		console.log('dat app!');
+
+
+		var defaultSize = 256;
+		var defaultPadding = '5';
+
+		var defaultBg = 'none';
+		var defaultFills = '#a94442,#31708f,#3c763d,#a6cee3,#b2df8a,#fb9a99,#e31a1c,#fdbf6f,#ff7f00,#cab2d6,#6a3d9a,#ffff99,#b15928'.split(',');
+
+		var defaultStroke = '#fff';
+		var defaultStrokeWidth = 1;
+		var defaultStrokeMode = '%';
+
+
+
+		$scope.extension = '.svg';
+		$scope.pieSlices = [4, 5, 6];
+
+		$scope.size = defaultSize;
+		$scope.padding = defaultPadding;
+
+		$scope.bgColor = defaultBg;
+		$scope.fillColors = defaultFills;
+
+		$scope.strokeColor = defaultStroke;
+		$scope.strokeWidth = defaultStrokeWidth;
+		$scope.strokeMode = defaultStrokeMode;
+
+
+		$scope.pieSrc = function() {
+			var src = '/';
+			var params = [];
+
+			if ($scope.size !== defaultSize) {
+				src += $scope.size + '/';
+			}
+
+			src += $scope.pieSlices + $scope.extension;
+
+			// params = setSize(params);
+			params = setBg(params);
+			params = setStrokeColor(params);
+			params = setStrokeWidth(params);
+
+			if (params.length) {
+				src += '?' + params.join('&');
+			}
+
+			return src;
+		};
+
+
+		$scope.deleteSlice = function(index) {
+			$scope.pieSlices.splice(index, 1);
+		};
+
+
+
+		function setBg(params) {
+			if ($scope.bgColor.length && $scope.bgColor !== defaultBg) {
+				var bgColor = $scope.bgColor.replace(/#/, '$');
+				params.push('bgColor=' + bgColor);
+			}
+
+			return params;
+		}
+
+
+		function setStrokeColor(params) {
+			if ($scope.strokeColor !== defaultStroke) {
+				var strokeColor = $scope.strokeColor.replace(/#/, '$');
+				params.push('stroke=' + strokeColor);
+			}
+			return params;
+		}
+
+
+		function setStrokeWidth(params) {
+			if ($scope.strokeWidth !== defaultStrokeWidth || $scope.strokeMode !== defaultStrokeMode) {
+				var strokeWidth = $scope.strokeWidth;
+				if ($scope.strokeMode === '%') {
+					strokeWidth += '$';
+				}
+
+				params.push('strokeWidth=' + strokeWidth);
+			}
+			return params;
+		}
+
+
+
+
+		function reduceArray(array, toSize) {
+			while (array.length > toSize) {
+				array.pop();
+			}
+			return array;
+		}
+
+		function expandArray(array, toSize) {
+			while (array.length < toSize) {
+				array.push(0);
+			}
+			return array;
+		}
+
+
+	}]);
